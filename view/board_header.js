8fb4ba5be72acfd43778c591087ad09185df6276
hansol cha <around_world@naver.com>
2016-01-04 17:22:53 +0900

    create board.html
    게시판 html

---
+++ view/board_header.js
@@ -0,0 +1,7 @@
+var line='<header id="board_header" class="jumbotron">'+
+		'<div class="container">'+
+			'<h2><a href="#">경영대학 선거관리 위원회 공고문</a></h2>'+//최신 공지사항을 넣어놓슴다
+			'<hr>'+
+			'<p>Donec id elit non mi porta gravida at eget metus.'+'</div>'+
+	'</header>';
+document.write(line);
