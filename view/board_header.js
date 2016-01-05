52b3850d5c36f8b77cdf15a5a0a96b71d7118602
hansol cha <around_world@naver.com>
2016-01-05 14:49:55 +0900

    board design/post
    ㅇ

--- view/board_header.js
+++ view/board_header.js
@@ -1,7 +1,7 @@
 var line='<header id="board_header" class="jumbotron">'+
 		'<div class="container">'+
-			'<h2><a href="#">경영대학 선거관리 위원회 공고문</a></h2>'+//최신 공지사항을 넣어놓슴다
+			'<h2><a href="post.html">경영대학 선거관리 위원회 공고문</a></h2>'+//최근 공지 올려놓기
 			'<hr>'+
-			'<p>Donec id elit non mi porta gravida at eget metus.'+'</div>'+
+			'<p>Donec id elit non mi porta gravida at eget metus.(요약)'+'</div>'+
 	'</header>';
 document.write(line);
