38c2b127bff439c36b045061a312a77b38199b67
EJShon <5on.3un.joo@gmail.com>
2016-01-29 17:34:10 +0900

    COMMIT

--- authen/models.py
+++ authen/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class users(models.Model):
-    user_id = models.IntegerField(primary_key=True)
+    user_id = models.CharField(primary_key=True, max_length=20)
     user_name = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     admission_year = models.DateField()
