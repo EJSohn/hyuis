a6ed3ddad49b517b87e1141f1aab522ee1d90ad1
EJShon <5on.3un.joo@gmail.com>
2016-01-29 15:36:59 +0900

    add users table

--- authen/models.py
+++ authen/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class users(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    user_name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    admission_year = models.DateField()
+    phone_number = models.CharField(max_length=20)
+    self_introduce = models.TextField(null=True)
+
+
