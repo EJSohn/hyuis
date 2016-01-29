bc6342da27795696f44854535f56063291035652
EJShon <5on.3un.joo@gmail.com>
2016-01-29 15:12:59 +0900

    back to standart django structure from google app engine structure

--- hyuis16/settings.py
+++ hyuis16/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authen',
+    'board',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'hyuis16.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -80,6 +82,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+]
 
 
 # Internationalization
--- hyuis16/urls.py
+++ hyuis16/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from hyuis16.views import index
+
 urlpatterns = [
+    url(r'^$', index),
+    #url('r^authen/', include('authen.urls')),
+    #url('r^board/', include('board.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
