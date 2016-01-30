b902f4eca4fee1f07068d9fe726fb39bc5296619
EJShon <5on.3un.joo@gmail.com>
2016-01-30 20:13:15 +0900

    full connection complete

---
+++ board/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.board ,name='board'),
+    url(r'^free/', views.free_board, name='free board'),
+    url(r'^review/', views.review, name='review'),
+    url(r'^review_post/', views.review_post, name='review_post'),
+    url(r'^post/', views.post, name='post'),
+
+]
--- board/views.py
+++ board/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render ,redirect
+from django.http import HttpResponse
 # Create your views here.
+
+def board(request):
+    return HttpResponse('board entire view')
+
+def free_board(request):
+    return render(request, 'board/board.html')
+
+def review(request):
+    return render(request, 'board/review.html')
+
+def review_post(request):
+    return render(request, 'board/review_post.html')
+
+def post(request):
+    return render(request, 'board/post.html')
