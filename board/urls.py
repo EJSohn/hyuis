0ced781ae57eed79513e76965890b20cae372f30
EJShon <5on.3un.joo@gmail.com>
2016-02-03 12:04:45 +0900

    pagination & url configuration updates

--- board/urls.py
+++ board/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.free_board ,name=''),
-    url(r'^entire_view/', views.free_board, name='entire_view'),
-    url(r'^notice/', views.free_board, name='notice'),
-    url(r'^free/', views.free_board, name='free board'),
+    url(r'^$', views.some_board ,name=''),
+    url(r'^entire_view/', views.entire_board, name='entire_view'),
+    url(r'^notice/([0-4]{1})/', views.some_board, name='notice'),
+    url(r'^free/([0-4]{1})/', views.some_board, name='free board'),
     url(r'^review/', views.review, name='review'),
     url(r'^review_post/', views.review_post, name='review_post'),
     url(r'^post/', views.post, name='post'),
--- board/views.py
+++ board/views.py
@@ -1,27 +1,63 @@
+# -*- coding: utf-8  -*-
 from django.shortcuts import render ,redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from datetime import datetime
 from .models import board, category
 from authen.models import hyu_users
 from django.core.urlresolvers import reverse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import board
 
 # Create your views here.
 
-def free_board(request):
-    return render(request, 'board/board.html')
+#전체게시판
+def entire_board(request):
+    all_list = board.objects.all()
+    paginator = Paginator(all_list, 10)
 
+    page = request.GET.get('page')
+    try:
+        all_post = paginator.page(page)
+    except PageNotAnInteger:
+        all_post = paginator.page(1)
+    except EmptyPage:
+        all_post = paginator.page(paginator.num_pages)
+
+    return render(request, 'board/board.html', {'posts':all_post})
+
+
+#게시판
+def some_board(request, category_id):
+    some_list = board.objects.all().filter(category_id=category_id)
+    paginator = Paginator(some_list, 10)
+
+    page = request.GET.get('page')
+    try:
+        some_post = paginator.page(page)
+    except PageNotAnInteger:
+        some_post = paginator.page(1)
+    except EmptyPage:
+        some_post = paginator.page(paginator.num_pages)
+
+    return render(request, 'board/board.html', {'posts':some_post})
+
+#후기게시판
 def review(request):
     return render(request, 'board/review.html')
 
+#후기게시글
 def review_post(request):
     return render(request, 'board/review_post.html')
 
+#일반게시글
 def post(request):
     return render(request, 'board/post.html')
 
+#글쓰기
 def write(request):
     return render(request, 'board/write.html')
 
+#내 페이지
 def mypage(request):
     return render(request, 'board/mypage.html')
 
