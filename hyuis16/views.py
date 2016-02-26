1ee35adcbaba3bcfe67ac06d51fa8bc5f6021c91
EJShon <5on.3un.joo@gmail.com>
2016-02-26 12:00:07 +0900

    commit

--- hyuis16/views.py
+++ hyuis16/views.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import hashlib
 
 def index(request):
-    notice = board.objects.all().filter(category_id=1).order_by('-post_id')[:6]
+    notice = board.objects.all().filter(category_id__parent=1).order_by('-post_id')[:6]
     free_board = board.objects.all().filter(category_id=2).order_by('-post_id')[:6]
     review = imghandler.objects.all().filter(post_id__category_id=4).order_by('-post_id')[:4]
     today = datetime.today().date()
